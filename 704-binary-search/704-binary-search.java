32a1dcda106f1991da78a423812416355ddb63f4
Woncheol Yoon <yoon1fe@naver.com>
2022-03-23 21:52:33 +0900

    Time: 0 ms (100.00%), Space: 54.8 MB (20.10%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+    
+    private int binarySearch(int[] nums, int target, int left, int right) {
+        if (left > right) return -1;
+        
+        int mid = (left + right) / 2;
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] < target) return binarySearch(nums, target, mid + 1, right);
+        else return binarySearch(nums, target, left, mid - 1);
+    }
+}
