e6eb0367cc61f1f0049f1c3ab18b85993e49944d
Woncheol Yoon <yoon1fe@naver.com>
2024-10-01 16:19:54 +0900

    Time: 4 ms (98.97%), Space: 56.8 MB (32.3%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+      int[] freq = new int[k];
+      for (int n : arr) {
+        int rem = n % k;
+        if (rem < 0) {
+          rem += k;
+        }
+        freq[rem]++;
+      }
+
+      if (freq[0] % 2 != 0) return false;
+      for (int i = 1; i <= k / 2; i++) {
+        if (freq[i] != freq[k - i]) return false;
+      }
+
+      return true;
+    }
+}
