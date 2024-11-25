7713fabfd82fdda5ff1a85f16170ef42ebcd3941
Woncheol Yoon <yoon1fe@naver.com>
2024-11-25 21:31:33 +0900

    Time: 0 ms (100%), Space: 50.6 MB (10.15%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+  public int singleNonDuplicate(int[] nums) {
+    int left = 0;
+    int right = nums.length - 1;
+
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (mid % 2 == 1) {
+        mid--;
+      }
+
+      if (nums[mid] != nums[mid + 1]) {
+        right = mid;
+      } else {
+        left = mid + 2;
+      }
+    }
+
+    return nums[right];
+  }
+}
